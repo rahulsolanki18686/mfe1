40dcb236f486a9770bc955c02e095d60432f299f
rahul solanki <rahulsolanki18686@gmail.com>
2023-10-10 21:12:34 +0530

    added auth module

--- packages/auth/config/webpack.dev.js
+++ packages/auth/config/webpack.dev.js
@@ -1,25 +1,26 @@
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin'); 
 const packageJson = require('../package.json');
+
 const devConfig = {
   mode: 'development',
   output: {
-    publicPath: 'http://localhost:8081/'
+    publicPath: 'http://localhost:8082/',
   },
   devServer: {
-    port: 8081,
+    port: 8082,
     historyApiFallback: {
       index: '/index.html',
     },
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'marketing',
+      name: 'auth',
       filename: 'remoteEntry.js',
       exposes: {
-        './MarketingApp': './src/bootstrap'
+        './AuthApp': './src/bootstrap',
       },
       shared: packageJson.dependencies,
     }),
--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -5,17 +5,21 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const packageJson = require('../package.json');
 const devConfig = {
   mode: 'development',
+  output: {
+    publicPath: 'http://localhost:8080/',
+  },
   devServer: {
     port: 8080,
     historyApiFallback: {
-      index: 'index.html',
+      index: '/index.html',
     },
   },
   plugins: [
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        marketing: 'marketing@http://localhost:8081/remoteEntry.js'
+        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+        auth: 'auth@http://localhost:8082/remoteEntry.js'
       },
       shared: ['react','react-dom'],
     }),
