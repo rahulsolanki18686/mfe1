3288bbd1c85646aa1d22bbc4f6ef81a5bee9ea55
rahul solanki <rahulsolanki18686@gmail.com>
2023-10-11 15:27:40 +0530

    dashboar intial

---
+++ packages/dashboard/config/webpack.common.js
@@ -0,0 +1,44 @@
+const {VueLoaderPlugin} = require('vue-loader');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+  resolve: {
+      extension: ['.js','.vue']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|woff|svg|eot|ttf)$/i,
+        use:[
+          {
+            loader: 'file-loader'
+          }
+        ]
+      },
+      {
+        test: /\.scss|\.css$/,
+        use: ['vue-style-loader','style-loader','css-loader','saas-loader']
+      },
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new VueLoaderPlugin()],
+};
